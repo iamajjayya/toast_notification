ca75ad9d2dd7378da51d97890ee6d28794fdab89
ajjayya <ajjayya2002@gmail.com>
2023-09-12 21:34:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let toastBox = document.getElementById("toastbox");
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfull submitted'
+let errorMsg = ' <i class="fa-solid fa-circle-xmark"></i> Please fix the error'
+let invalidMsg = ' <i class="fa-solid fa-circle-exclamation"></i>Invalid submission'
+
+
+
+
+
+function showToast(msg) {
+    let toast = document.createElement("div");
+    toast.classList.add("toast");
+    toast.innerHTML =msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('error')){
+        toast.classList.add("error")
+    }
+
+    if(msg.includes('Invalid')){
+        toast.classList.add("invalid")
+    }
+
+    setTimeout(() =>{
+        toast.remove();
+    },6000)
+}
+
